26be13ae72a317337a9c126b0469c12b65400ed4
Patrick Hall <pathall@gmail.com>
2024-05-15 10:40:17 -0400

    Standardize on a /components/css/component-docs.css pattern. good?

--- generate-component-docs-css.js
+++ generate-component-docs-css.js
@@ -2,6 +2,6 @@
 /* sample-component/sample-component-docs.css */
 export let generateComponentDocsCss = ({slug, componentName}) => `
 @import url(${slug}.css);
-@import url(https://docling.land/docs.css);
+@import url(../css/component-docs.css);
 `
 
